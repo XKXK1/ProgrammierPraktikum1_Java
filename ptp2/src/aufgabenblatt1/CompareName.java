b8ba2c6ea27ffae7b1ead80cc01c4c879e81bed1
XKXK1 <deryauyargil@hotmail.de>
2016-09-25 09:54:18 +0200

    compareToUpperCase -> compareTo

--- ptp2/src/aufgabenblatt1/CompareName.java
+++ ptp2/src/aufgabenblatt1/CompareName.java
@@ -6,11 +6,11 @@ public class CompareName implements Comparator<Student> {
 
 	@Override
 	public int compare(Student student1, Student student2) {
-		if (student1.getVorname().compareToIgnoreCase(student2.getVorname()) < 0){
+		if (student1.getVorname().compareTo(student2.getVorname()) < 0){
 			return -1;
 		}
 		
-		if (student1.getVorname().compareToIgnoreCase(student2.getVorname()) > 1){
+		if (student1.getVorname().compareTo(student2.getVorname()) > 1){
 			return 1;
 		}
 		return 0;
