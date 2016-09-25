8ae9d0a358635c06b7e47f4efb609947d71e2ed2
XKXK1 <deryauyargil@hotmail.de>
2016-09-25 09:55:45 +0200

    "private" modifier vor deklaration gesetzt

--- ptp2/src/aufgabenblatt1/Pruefungsleistung.java
+++ ptp2/src/aufgabenblatt1/Pruefungsleistung.java
@@ -1,8 +1,8 @@
 package aufgabenblatt1;
 
 public class Pruefungsleistung {
-	int note;
-	String modulname;
+	private int note;
+	private String modulname;
 	
 	public Pruefungsleistung(int note, String modulname){
 		this.note = note;
