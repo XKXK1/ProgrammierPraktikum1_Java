412f20ea038e84ef008ca7ec1db7367ecc02b776
XKXK1 <deryauyargil@hotmail.de>
2016-09-25 09:21:54 +0200

    Erste Version(unvollstaendig)
    Comparator CompareName noch fehlerhaft

---
+++ ptp2/src/aufgabenblatt1/Pruefungsleistung.java
@@ -0,0 +1,12 @@
+package aufgabenblatt1;
+
+public class Pruefungsleistung {
+	int note;
+	String modulname;
+	
+	public Pruefungsleistung(int note, String modulname){
+		this.note = note;
+		this.modulname = modulname;
+	}
+
+}
